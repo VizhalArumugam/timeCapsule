b9440566ec3132c3e591d56b1e1d34cca2091f29
Harsith2k5 <23z116@psgitech.ac.in>
2024-12-15 11:39:10 +0530

    Modified background in Main page and set the text area to retro image background

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,28 @@
-import React from "react";
-import futuristicVid from "../videos/futuristic.mp4"; // Import video properly
+import React, { useEffect, useState } from "react";
+import futuristicVid from "../videos/futuristic.mp4"; 
 import "../styles/main1.css";
+import retroImg from "../images/retroCard.jpg";
 
 export default function Main() {
+    const [content, setContent] = useState("Dear future, Harsith!"); 
+    const [isDateAppended, setIsDateAppended] = useState(false); 
+
+    function getDateTime() {
+        const currentDate = new Date();
+        return currentDate.toLocaleString(); 
+    }
+
+    useEffect(() => {
+        if (!isDateAppended) {
+            setContent(prevContent => prevContent + `\n${getDateTime()}`); 
+            setIsDateAppended(true); 
+        }
+    }, [isDateAppended]); 
+
+    const handleChange = (e) => {
+        setContent(e.target.value); 
+    };
+
     return (
         <div className="outer-container">
             <div className="options-container">
@@ -10,11 +30,15 @@ export default function Main() {
                     Your browser does not support the video tag.
                 </video>
             </div>
-            <div className="container">
-                <textarea name="text" id="text" className="area"></textarea>
+            <div className="retro-card">
+                <textarea
+                    name="retro"
+                    id="retro"
+                    value={content} 
+                    onChange={handleChange} 
+                />
                 <button>Send to Future</button>
             </div>
-            
         </div>
     );
 }
